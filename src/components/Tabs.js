782309eb55fa200bace61a34873618b6366ece3c
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-22 16:09:08 -0400

    Tabs Component Added

---
+++ src/components/Tabs.js
@@ -0,0 +1,60 @@
+import React from "react"
+import CurrentWeather from "../screens/CurrentWeather"
+import UpcomingWeather from "../screens/UpcomingWeather"
+import City from "../screens/City"
+// 2:34:10 platform specific tab navigator
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from '@expo/vector-icons'
+
+const Tab = createBottomTabNavigator()
+
+const Tabs = () => {
+    return(
+        <Tab.Navigator 
+            screenOptions={{
+                tabBarActiveTintColor: 'tomato',
+                tabBarInactiveTintColor: 'grey'
+            }}
+        >
+            <Tab.Screen 
+                name={'Current'} 
+                component={CurrentWeather} 
+                // focused: different icons when active and inactive
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Feather 
+                            name={'droplet'} 
+                            size={25} 
+                            color={focused ? 'tomato' : 'black'}
+                        />
+                    )
+                }}/>
+            <Tab.Screen 
+                name={'Upcoming'} 
+                component={UpcomingWeather}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Feather 
+                            name={'clock'} 
+                            size={25} 
+                            color={focused ? 'tomato' : 'black'}
+                        />
+                )
+            }}/>
+            <Tab.Screen 
+                name={'City'} 
+                component={City}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Feather 
+                            name={'home'} 
+                            size={25} 
+                            color={focused ? 'tomato' : 'black'}
+                        />
+                )
+            }}/>
+      </Tab.Navigator>
+    )
+}
+
+export default Tabs
