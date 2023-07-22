0a6ca82d86a5d2e04e015d980a95f9869937e702
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-22 13:58:05 -0400

    IconText Component boilerplate

---
+++ src/components/IconText.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View,StyleSheet } from "react-native";
+import { Feather } from '@expo/vector-icons'
+
+const IconText = () => {
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default IconText
