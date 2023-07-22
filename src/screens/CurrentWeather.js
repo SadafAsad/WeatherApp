45caf5c454a884c16cb82d835e22ea77f21f811c
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-22 14:49:58 -0400

    small UI fix

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -36,7 +36,8 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   temp: {
     color: 'black',
