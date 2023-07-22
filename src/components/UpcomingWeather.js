d10124f9a24f419baa2c23bb2646664bd8f2fc04
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-22 12:49:06 -0400

    UpcomingWeather Component showing a list

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,17 +1,109 @@
 import React from "react"
-import { SafeAreaView, StyleSheet, Text } from "react-native"
+import { SafeAreaView, StyleSheet, Text, FlatList, View, StatusBar } from "react-native"
+import { Feather } from '@expo/vector-icons'
+
+const DATA = [
+    {
+        dt_txt: "2023-02-18 12:00:00",
+        main: {
+            temp_max: 8.55,
+            temp_min: 7.55
+        },
+        weather: [
+            {
+                main: 'Clear'
+            }
+        ]
+    },
+    {
+        dt_txt: "2023-02-18 15:00:00",
+        main: {
+            temp_max: 8.55,
+            temp_min: 7.55
+        },
+        weather: [
+            {
+                main: 'Clouds'
+            }
+        ]
+    },
+    {
+        dt_txt: "2023-02-18 18:00:00",
+        main: {
+            temp_max: 8.55,
+            temp_min: 7.55
+        },
+        weather: [
+            {
+                main: 'Rain'
+            }
+        ]
+    }
+]
+
+// each list's elements
+const Item = (props) => {
+    const { dt_txt, min, max, condition } = props
+    return (
+        <View style={styles.item}>
+            <Feather name={'sun'} size={50} color={'white'} />
+            <Text style={styles.date}>{dt_txt}</Text>
+            <Text style={styles.temp}>{min}</Text>
+            <Text style={styles.temp}>{max}</Text>
+        </View>
+    )
+}
 
 const UpcomingWeather = () => {
+    const renderItem = ({ item }) => (
+        <Item 
+            condition={item.weather[0].main} 
+            dt_txt={item.dt_txt} 
+            min={item.main.temp_min} 
+            max={item.main.temp_max} 
+        />
+    )
     return (
         <SafeAreaView style={styles.container}>
             <Text> Upcoming Weather </Text>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                // reason for key is performance based
+                // keep track of each item in the list
+                // hence, updating the list rather than rebuilding evrything when a change happens
+                keyExtractor={(item) => item.dt_txt}
+            />
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        // taking into account different platforms
+        // initialize with the current height of the status bar for different platforms
+        marginTop: StatusBar.currentHeight || 0,
+        backgroundColor: 'red'
+    },
+    item: {
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        // to evenly distribute items in the row
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        borderWidth: 5,
+        backgroundColor: 'pink'
+    },
+    temp: {
+        color: 'white',
+        fontSize: 20
+    },
+    date: {
+        color: 'white',
+        fontSize: 15
     }
 })
 
