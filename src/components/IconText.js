29cf0f23cf4108e6bf2f84c5454a733d5e94e4df
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-22 14:17:11 -0400

    Code Clean Up

--- src/components/IconText.js
+++ src/components/IconText.js
@@ -2,16 +2,24 @@ import React from "react";
 import { Text, View,StyleSheet } from "react-native";
 import { Feather } from '@expo/vector-icons'
 
-const IconText = () => {
+const IconText = (props) => {
+    const { iconName, iconColor, bodyText, bodyTextStyles } = props
+    const { container, textTheme } = styles
     return(
-        <View>
-
+        <View style={container}>
+            <Feather name={iconName} size={50} color={iconColor} />
+            <Text style={[textTheme, bodyTextStyles]}>{bodyText}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    textTheme: {
+        fontWeight: 'bold'
+    },
+    container: {
+        alignItems: 'center',
+    }
 })
 
 export default IconText
