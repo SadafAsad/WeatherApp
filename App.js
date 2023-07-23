5bebeb55d27e2a2396f06705a32d0f84f100a852
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-23 11:53:28 -0400

    env set up

--- App.js
+++ App.js
@@ -3,11 +3,16 @@ import { ActivityIndicator, View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
 import * as Location from 'expo-location'
+import { TEST_KEY } from '@env'
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
   const [loading, setLoading] = useState(true)
   const [location, setLocation] = useState(null)
   const [error, setError] = useState(null)
+
+  console.log(TEST_KEY)
 
   useEffect(() => {
     ;(async () => {
