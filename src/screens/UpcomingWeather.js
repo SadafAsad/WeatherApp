deb263e86babe1030b55660daed9ba93ef9ccb56
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-25 19:51:01 -0400

    Status Bar & Tab Bar Styles -- extended

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -7,6 +7,8 @@ import {
   ImageBackground
 } from 'react-native'
 import ListItem from '../components/ListItem'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import FocusAwareStatusBar from '../components/StatusBar'
 
 const UpcomingWeather = ({ weatherData }) => {
   const renderItem = ({ item }) => (
@@ -17,39 +19,40 @@ const UpcomingWeather = ({ weatherData }) => {
       max={item.main.temp_max}
     />
   )
-  const { container, image } = styles
+  const { image } = styles
+  const insets = useSafeAreaInsets()
 
   return (
-    <SafeAreaView style={container}>
-      <ImageBackground
-        // props are used to pass data from parent to child
-        // they can be used to customize our components
-        // core components usually come with props which can be used
-        // prop source here is used to set the image
-        source={require('../../assets/upcoming-background.jpg')}
-        style={image}
-      >
-        <FlatList
-          data={weatherData}
-          renderItem={renderItem}
-          // reason for key is performance based
-          // keep track of each item in the list
-          // hence, updating the list rather than rebuilding evrything when a change happens
-          keyExtractor={(item) => item.dt_txt}
-        />
-      </ImageBackground>
-    </SafeAreaView>
+    <ImageBackground
+      // props are used to pass data from parent to child
+      // they can be used to customize our components
+      // core components usually come with props which can be used
+      // prop source here is used to set the image
+      source={require('../../assets/upcoming-background.jpg')}
+      style={[
+        image,
+        {
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+          paddingLeft: insets.left,
+          paddingRight: insets.right
+        }
+      ]}
+    >
+      <FocusAwareStatusBar barStyle="light-content" />
+      <FlatList
+        data={weatherData}
+        renderItem={renderItem}
+        // reason for key is performance based
+        // keep track of each item in the list
+        // hence, updating the list rather than rebuilding evrything when a change happens
+        keyExtractor={(item) => item.dt_txt}
+      />
+    </ImageBackground>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // taking into account different platforms
-    // initialize with the current height of the status bar for different platforms
-    marginTop: StatusBar.currentHeight || 0,
-    backgroundColor: 'royalblue'
-  },
   image: {
     flex: 1
   }
