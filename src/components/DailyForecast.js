e9279bff5736fcca4950ff996a31f9b82721b881
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-27 13:21:49 -0400

    daily forecast set up

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -1,14 +1,95 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, FlatList, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons'
 
 const DailyForecast = () => {
-  return (
-    <View>
-      <Text>hour</Text>
-      <Text>icon</Text>
-      <Text>temp</Text>
+  let DATA = [
+    {
+      hour: '4',
+      icon: 'sun',
+      temp: '20•'
+    },
+    {
+      hour: '5',
+      icon: 'sun',
+      temp: '20•'
+    },
+    {
+      hour: '6',
+      icon: 'sun',
+      temp: '20•'
+    },
+    {
+      hour: '7',
+      icon: 'sun',
+      temp: '20•'
+    },
+    {
+      hour: '8',
+      icon: 'sun',
+      temp: '20•'
+    },
+    {
+      hour: '9',
+      icon: 'sun',
+      temp: '20•'
+    },
+    {
+      hour: '1',
+      icon: 'sun',
+      temp: '20•'
+    },
+    {
+      hour: '2',
+      icon: 'sun',
+      temp: '20•'
+    }
+  ]
+
+  const renderItem = ({ item }) => (
+    <View style={styles.eachRow}>
+      <Text style={styles.hourStyle}>{item.hour}</Text>
+      <Feather name="sun" size={35} color="black" />
+      <Text style={styles.tempStyle}>{item.temp}</Text>
     </View>
   )
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.hour}
+        />
+      </View>
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.67,
+    backgroundColor: 'rgba(225, 225, 255, 0.5)',
+    borderRadius: 20,
+    margin: 10,
+    paddingLeft: 60,
+    paddingRight: 60,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  eachRow: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  hourStyle: {
+    fontSize: 20,
+    marginRight: 10
+  },
+  tempStyle: {
+    fontSize: 20,
+    marginLeft: 10
+  }
+})
 
 export default DailyForecast
