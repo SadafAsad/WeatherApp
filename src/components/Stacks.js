7bd29d1110631a398151b698b8b27f4aa8e6b5d6
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-27 14:59:36 -0400

    Trying to make the swipping thing work

--- src/components/Stacks.js
+++ src/components/Stacks.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import CurrentWeather from '../screens/CurrentWeather'
 import moment from 'moment'
 import UpcomingWeather from '../screens/UpcomingWeather'
 import City from '../screens/City'
-import { PanGestureHandler, State } from 'react-native-gesture-handler'
+import { TabView, SceneMap } from 'react-native-tab-view'
 
 const Stack = createNativeStackNavigator()
+
+const MyTabView = () => {
+  const [index, setIndex] = useState(0)
+  const routes = [
+    { key: 'Current', title: 'Current' },
+    { key: 'Upcoming', title: 'Upcoming' },
+    { key: 'City', title: 'City' }
+  ]
+  const renderScene = SceneMap({
+    screenA: 'Current',
+    screenB: 'Upcoming',
+    screenC: 'City'
+  })
+  const handleIndexChange = (newIndex) => {
+    setIndex(newIndex)
+  }
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={handleIndexChange}
+      swipeEnabled={true}
+    />
+  )
+}
 
 const Stacks = ({ weather }) => {
   return (
@@ -20,6 +45,11 @@ const Stacks = ({ weather }) => {
       }}
       initialRouteName="Current"
     >
+      <Stack.Screen
+        name="Main"
+        component={MyTabView}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen name="Current">
         {() => (
           <CurrentWeather
